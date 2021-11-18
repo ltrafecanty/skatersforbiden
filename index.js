4af01bdaba06ef11d6a5ddc5b8742c1bf446fa1f
ltrafecanty <ltrafeca@ucsc.edu>
2021-11-17 20:49:50 -0400

    registration resolver

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
-const typeDefs = gql`
-  type Query {
-    sayHi: String!
-  }
-`;
-const resolvers = {
-  Query: {
-    sayHi: () => "Hello World!!!",
-  },
-};
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
-  .then(() => {
-      console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  });
+    .connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log("MongoDB Connected");
+        return server.listen({ port: 5000 });
+    })
+    .then((res) => {
+        console.log(`Server running at ${res.url}`);
+    });
