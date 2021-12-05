cafe62fa043a8e8f5d7ecae1764efff5bc4a486b
ltrafecanty <ltrafeca@ucsc.edu>
2021-12-05 16:48:03 -0400

    Homepage getPosts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import 'fomantic-ui-css/semantic.css';
+import './App.css';
+import { Container } from 'semantic-ui-react';
+
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
+
 function App() {
-  return (
-    <div>
-      {' '}
-      <hi> Hello World </hi>{' '}
-    </div>
-  );
+   return (
+      <Router>
+         <Container>
+            <MenuBar />
+            <Routes>
+               <Route path='/' element={<Home />} />
+               <Route path='/login' element={<Login />} />
+               <Route path='/register' element={<Register />} />
+            </Routes>
+         </Container>
+      </Router>
+   );
 }
 
 export default App;
