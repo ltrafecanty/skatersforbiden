f00da8a9d4b38fa1d1038e8ddc224ea59821794b
ltrafecanty <ltrafeca@ucsc.edu>
2021-12-20 20:40:51 -0400

    a

--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,8 @@ function App() {
                <MenuBar />
                <Routes>
                   <Route path='/' component={<Home />} />
-                  <AuthRoute path='/login' component={<Login />} />
-                  <AuthRoute path='/register' component={<Register />} />
+                  <Route path='/login' component={<Login />} />
+                  <Route path='/register' component={<Register />} />
                </Routes>
             </Container>
          </Router>
