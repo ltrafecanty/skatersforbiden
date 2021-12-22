c701a7e28b71f08fcabae4e19f698647ea6e93d7
ltrafecanty <ltrafeca@ucsc.edu>
2021-12-21 21:02:10 -0400

    what

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
                <MenuBar />
                <Routes>
                   <Route path='/' component={<Home />} />
-                  <Route path='/login' component={<Login />} />
+                  <AuthRoute path='/login' component={<Login />} />
                   <Route path='/register' component={<Register />} />
                </Routes>
             </Container>
