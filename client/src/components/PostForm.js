08614192ab61645e0dcb1ee4605500fbc0b000d7
ltrafecanty <ltrafeca@ucsc.edu>
2022-01-13 19:50:36 -0400

    posts not refreshing upon submit

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import { useMutation } from '@apollo/react-hooks';
 
 import { useForm } from '../util/hooks';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function PostForm() {
    const { values, onChange, onSubmit } = useForm(createPostCallback, {
@@ -12,8 +13,12 @@ function PostForm() {
 
    const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
       variables: values,
-      update(_, result) {
-         console.log(result);
+      update(proxy, result) {
+         const data = proxy.readQuery({
+            query: FETCH_POSTS_QUERY,
+         });
+         data.getPosts = [result.data.createPost, ...data.getPosts];
+         proxy.writeQuery({ query: FETCH_POSTS_QUERY });
          values.body = '';
       },
    });
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
 import { Grid } from 'semantic-ui-react';
 
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
 import { AuthContext } from '../context/auth';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
 
 function Home() {
    const { user } = useContext(AuthContext);
@@ -21,11 +21,12 @@ function Home() {
             <h1> Recent Posts </h1>{' '}
          </Grid.Row>{' '}
          <Grid.Row>
+            {' '}
             {user && (
                <Grid.Column>
                   <PostForm />
                </Grid.Column>
-            )}
+            )}{' '}
             {loading ? (
                <h1> Loading posts... </h1>
             ) : (
@@ -41,25 +42,4 @@ function Home() {
    );
 }
 
-const FETCH_POSTS_QUERY = gql`
-   {
-      getPosts {
-         id
-         body
-         createdAt
-         username
-         likeCount
-         likes {
-            username
-         }
-         commentCount
-         comments {
-            id
-            username
-            createdAt
-            body
-         }
-      }
-   }
-`;
 export default Home;
